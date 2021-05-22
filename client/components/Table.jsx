9edd60d22c7e5940b6d31e0a3b9b871b440b5fd6
lawrencehan650 <lawrencehan3@gmail.com>
2021-05-21 17:14:53 -0700

    boiler plate

--- client/components/Table.jsx
+++ client/components/Table.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Table extends Component {
   render() {
     return(
-      <div>this is table</div>
+      <div>this is another react component</div>
     )
   }
 }
