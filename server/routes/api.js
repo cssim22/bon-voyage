e5d03905cb9eb597ccbf2db0fa4dbc4670c3d20b
Cameron Simmons <cssim22@gmail.com>
2021-05-22 17:36:20 -0700

    have a working post method in server that connects to database

---
+++ server/routes/api.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const express = require('express');
+
+//middleware
+const tripController = require('../controllers/tripController');
+const app = express.Router();
+
+
+
+//post request
+app.post('/new-trip',tripController.addTrip, (req,res) => {
+  return res.status(200).send('successfully added trip');
+});
+
+
+//link to data
+//POST request
+//db query
+//form data from app and insert into proper table on SQL database
+//test request with Postman
+//use middleware
+
+
+
+//catch-all route for unhandled requests
+
+app.use(function (req, res) {
+    res.status(404).sendFile(path.join(__dirname, '.', 'client', '404.html'));
+    
+});
+
+module.exports = app;
